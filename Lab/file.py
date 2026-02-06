f596d8968ba81299b7f8196cbc2061c1f53c5348
Evelyn Barragan <barragan2020eb@outlook.com>
2026-02-05 20:26:57 -0800

    create file.py

---
+++ Lab/file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 20:15:02 2026
+
+@author: evebarr20
+"""
+# import easygui
+import easygui as g
+
+def file_choose():
+    """
+    open file browser window and allow user to select any file
+
+    Returns
+    -------
+    filename : STR
+        returns file path the user selects.
+
+    """
+    title = "Please select a File"
+    return g.fileopenbox(title) 
+
+print(file_choose())
